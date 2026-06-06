2ad8bebadd722b2d780f0cc3f918c6404df636d2
VladimirYastrebov <vladimir.i.yastrebov@gmail.com>
2026-06-06 16:07:39 +0300

    feat: import i18n to both projects

--- frontend/src/fsp-app/mainfsp.jsx
+++ frontend/src/fsp-app/mainfsp.jsx
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter } from "react-router";
 import App from "./App";
+import "./shared/i18n";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
--- frontend/src/kanban-app/mainKanban.tsx
+++ frontend/src/kanban-app/mainKanban.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BoardProvider } from "./contexts/BoardContext.tsx";
+import "./shared/i18n";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
